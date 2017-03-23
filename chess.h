af05ebfaa79ad67da01cb4bfb41eab4ff5e4050f
Sam Hoang <samhv.ict@gmail.com>
2017-03-23 23:54:18 +0700

    Add mode ACK, ERR for reply, update Piece struct

--- chess.h
+++ chess.h
@@ -5,4 +5,32 @@
 #ifndef PROJECTNP_CHESS_H
 #define PROJECTNP_CHESS_H
 
+// location
+typedef struct loc_s {
+    unsigned int x : 3;
+    unsigned int y : 3;
+} Loc;
+
+
+typedef enum piecestype_e {
+
+    KING, QUEEN, ROOK, KNIGHT, BISHOP, PAWN
+
+} PieceType;
+
+typedef enum team_e {
+    BLACK, WHITE
+} Team;
+
+typedef enum status_e {
+    DEAD, ALIVE
+} Status;
+
+typedef struct piece_s {
+    PieceType pieceType;
+    Loc currentLoc;
+    Team team;
+    Status status;
+} Piece;
+
 #endif //PROJECTNP_CHESS_H
--- protocol.h
+++ protocol.h
@@ -5,4 +5,23 @@
 #ifndef PROJECTNP_PROTOCOL_H
 #define PROJECTNP_PROTOCOL_H
 
+#include "chess.h"
+
+typedef enum mode_e {
+    JOIN,
+    MOVE,
+    RESIGN,
+    RESTART,
+    ACK,
+    ERR
+} Mode;
+
+typedef struct protocol_s {
+    Mode mode;
+
+    Loc from;
+    Loc to;
+
+} Protocol;
+
 #endif //PROJECTNP_PROTOCOL_H
