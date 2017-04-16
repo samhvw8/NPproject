4d603fe0afb60a6fb40fb5775ea5c2ec4a426c66
Sam Hoang <samhv.ict@gmail.com>
2017-04-16 13:52:05 +0700

    - another_player_move since it not use

--- handle.h
+++ handle.h
@@ -98,8 +98,6 @@ void change_game_status(GameState gameState);
 
 void send_to_player(Mode mode,int x, int y, int i, int j);
 
-void another_player_move(int i, int j, int x, int y);
-
 void rev_from_player(int signo);
 
 void change_team(Team team);
